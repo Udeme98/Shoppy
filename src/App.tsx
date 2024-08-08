18a1f51bd3bba5712e3caf03ebcc6612c44aa26d
Udeme98 <Darealudeme1@gmail.com>
2024-08-08 10:59:38 +0100

    routing setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,18 @@
+import { Route, Routes } from "react-router-dom";
+import { Container } from "react-bootstrap";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Store from "./pages/Store";
+
 const App = () => {
-  return <div>Shoppy</div>;
+  return (
+    <Container className="mb-4">
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/store" element={<Store />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
+    </Container>
+  );
 };
 export default App;
---
+++ src/pages/Store.tsx
@@ -0,0 +1,4 @@
+const Store = () => {
+  return <div>Store Page</div>;
+};
+export default Store;
