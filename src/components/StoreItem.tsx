3e04808f66294a29fc47e4bf4850b24461d4bf23
Udeme98 <Darealudeme1@gmail.com>
2024-08-11 15:46:01 +0100

    shopping cart context setup. getItemQuantity function

--- src/components/StoreItem.tsx
+++ src/components/StoreItem.tsx
@@ -1,5 +1,6 @@
 import { Button, Card } from "react-bootstrap";
 import formatCurrency from "../utilities/formatCurrency";
+import { useShopping } from "../context/ShoppingCartContext";
 
 type StoreItemProps = {
   id: number;
@@ -9,7 +10,14 @@ type StoreItemProps = {
 };
 
 const StoreItem = ({ id, name, price, imgUrl }: StoreItemProps) => {
-  const quantity = 0;
+  const {
+    getItemQuantity,
+    increaseCartQuantity,
+    decreaseCartQuantity,
+    removeFromCart,
+  } = useShopping();
+  const quantity = getItemQuantity(id);
+
   return (
     <Card className="h-100">
       <Card.Img
