b2b512b4960774bc9636da417fb6a9267e1cdacc
Udeme98 <Darealudeme1@gmail.com>
2024-08-14 02:32:37 +0100

    Offset component to showcase the cart items

--- src/components/ShoppingCart.tsx
+++ src/components/ShoppingCart.tsx
@@ -1,4 +1,18 @@
-const ShoppingCart = () => {
-  return <div>ShoppingCart</div>;
+import { Offcanvas } from "react-bootstrap";
+import { useShopping } from "../context/ShoppingCartContext";
+
+type ShoppingCartProp = {
+  isOpen: boolean;
+};
+
+const ShoppingCart = ({ isOpen }: ShoppingCartProp) => {
+  const { closeCart } = useShopping();
+  return (
+    <Offcanvas show={isOpen} onHide={closeCart} placement="end">
+      <Offcanvas.Header closeButton>
+        <Offcanvas.Title>Cart</Offcanvas.Title>
+      </Offcanvas.Header>
+    </Offcanvas>
+  );
 };
 export default ShoppingCart;
